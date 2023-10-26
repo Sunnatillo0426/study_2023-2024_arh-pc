1f7c1cf09b970492c127436c3671d2c9ce7391e0
Sunnatillo0426 <1132224173@pfur.ru>
2023-10-26 10:14:05 +0300

    add files

---
+++ labs/lab04/hello.asm
@@ -0,0 +1,19 @@
+; hello.asm                             
+SECTION .data                        ;Начало секции данных
+  hello:        DB 'Hello world!',10 ; 'Hello word! плюс
+                                     ;симбол перевода строки
+  helloLen:  EQU $-hello             ; Длина строки hello
+  
+  SECTION .text      ;начало секции кода
+     GLOBAL _start
+     
+   _start:           ;Точка входа в программу 
+       mov eax,4        ; Точка входа в программу             
+       mov ebx,1        ; Системный вызов для записи (sys_write)
+       mov ecx,hello    ; Описател файла '1' - стандартный вывод
+       mov edx,helloLen ; Адрес строки hello
+       int 80h          ; вызов ядра
+       
+       mov eax,1        ; Системный вызов для выхода (system_exit)
+       mov ebx,0        ; Вывод с кодом воздрата '0' (без ошибок)
+       int 80h          ; Вызов ядра
